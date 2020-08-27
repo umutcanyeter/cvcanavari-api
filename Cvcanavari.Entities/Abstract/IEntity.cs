39fdcbfeab385173a8fa4d3613dfb1fc5c365693
umutcanyeter <umutcanyeter@outlook.com>
2020-08-27 23:40:26 +0300

    First commit.

---
+++ Cvcanavari.Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cvcanavari.Entities.Abstract
+{
+    public interface IEntity
+    {
+        // Base for entities.
+    }
+}
